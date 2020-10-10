c02e2bb6283f11545083d7149b93374972d71fcf
haritskoding <haritscoding@gmail.com>
2020-10-10 07:05:49 +0700

    ad db

--- config/database.php
+++ config/database.php
@@ -50,7 +50,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'laravel6_cruds'),
             'username' => env('DB_USERNAME', 'aris23'),
-            'password' => env('DB_PASSWORD', ''),
+            'password' => env('DB_PASSWORD', '069f2c33'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
